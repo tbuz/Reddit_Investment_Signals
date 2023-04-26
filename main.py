d284ea4aecb0464ad68c0eecc95da7fa53e0aea4
moritzschneider <moritz86.ms@gmail.com>
2023-04-26 21:12:49 +0200

    setup complete - TODOs: Dataset

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 #Hey and welcome to Reddit Investment Signals
 from organiser import organiser_class
 
-
 # set your PARAMETER here
+# in days (1, 3, 7, 30, 90)
 time_horizon = 3
+# start and end-date in DD-MM-YYYY
 start_date = '01-01-2021'
 end_date = '01-04-2021'
-
+# target (see Read_me or Paper)
+target = 'target_1'
 
 #if you set your parameter press play!
 if __name__ == '__main__':
-    example_instance = organiser_class(time_horizon, start_date, end_date)
+    example_instance = organiser_class(time_horizon, start_date, end_date, target)
     example_instance.print_info()
+    example_instance.get_setup()
--- organiser.py
+++ organiser.py
@@ -1,14 +1,22 @@
 #import pre_Training
 #import xgBoost
 #import feature_engineering
+from setup import first_installer
 
 class organiser_class:
-    def __init__(self, time_horizon, start_date, end_date):
+    def __init__(self, time_horizon, start_date, end_date, target):
         self.time = time_horizon
         self.start_date = start_date
         self.end_date = end_date
+        self.target = target
 
     def print_info(self):
         print(f"The time horizon is {self.time} days.")
         print(f"The start date is {self.start_date}.")
         print(f"The end date is {self.end_date}.")
+        print(f"The target is {self.target}.")
+
+    def get_setup(self):
+        print('Installing')
+        first = first_installer()
+        first.install_first_time()
